17e59b54309ccd435b3250a9ed77d1e52077c3fc
Francisco Guzman <28841275+fguzmang23@users.noreply.github.com>
2022-10-17 17:58:08 -0400

    using useRef hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useRef } from 'react'
 import './App.css'
 import Homepage from "./pages/Homepage/index.tsx"
 
 function App() {
+  const inputRef = useRef(null)
+
+
+  const onClickHandler = () => {
+    console.log(inputRef.current.value)
+    inputRef.current.focus()
+  }
+
   return (
-    <Homepage/>
+    <>
+    <h1>the header</h1>
+    <p>the paragraph</p>
+
+    <input ref={inputRef} type="text"/>
+    
+    <button onClick={onClickHandler}> click me</button>
+    </>
   )
 }
 
