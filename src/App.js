060654efce3a3bbe04f5a81bacbb42b968309111
Francisco Guzman <28841275+fguzmang23@users.noreply.github.com>
2022-10-26 10:14:36 -0400

    lodash.debounce performs better than useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css'
 import Homepage from "./pages/Homepage/index.tsx"
 // import WebWorkerImplementation from "./components/WebWorkerImplementation/index.tsx"
+import NRHI from "./components/NewReactHooksImplementation/index.tsx"
+
 
 function App() {
   return (
-    <Homepage/>
+    // <Homepage/>
+    <NRHI/>
   )
 }
 
